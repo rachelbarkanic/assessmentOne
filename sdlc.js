95b04d96350b455fdd020c6eedfc4c07e4fe08d9
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-25 12:48:18 -0700

    second step defined

--- sdlc.js
+++ sdlc.js
@@ -19,3 +19,10 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step in which you sit down and think about all of the small things that will fit together to accomplish the goal"
+const design = ""
+const implementation = ""
+
+const maintain = ""
+
+const testingAndIntegration = ""
