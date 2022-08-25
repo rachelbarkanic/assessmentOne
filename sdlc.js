4ffaebdb4a29b7c2fbb12b7f54a30efeb05bd5e6
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-25 12:51:25 -0700

    step 3

--- sdlc.js
+++ sdlc.js
@@ -19,10 +19,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysisOfRequirements = "This is the second step in which you sit down and think about all of the small things that will fit together to accomplish the goal"
-const design = ""
+
+const analysisOfRequirements = "This is the second step in which you sit down and think about all of the small things that will fit together to accomplish the goal."
+
+const design = "This is the third step in which the user experience is decided upon and what, for example, the website will look like."
+
 const implementation = ""
 
-const maintain = ""
-
 const testingAndIntegration = ""
+
+const maintain = ""
