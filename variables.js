15d59a46a715394e263bea5e5e039c0dbb1f3ae9
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-25 17:37:50 -0700

    problem 2

--- variables.js
+++ variables.js
@@ -6,7 +6,7 @@ let myName = 'Rachel'
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let faveNum = 10
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
