18e2578695244c96e2d17d5db517432fad516e27
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-27 13:39:57 -0700

    assessment before extra credit

--- arrays.js
+++ arrays.js
@@ -35,9 +35,14 @@ console.log(middleNums);
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-let answers = []
+let answers = [];
 
 for (let i = 0; i < bigOrSmallArray.length; i++) {
-  if (bigOrSmallArray[i] > 100) 
-    answers.push('big') }
+  if (bigOrSmallArray[i] > 100)
+    answers.push('big'); 
+else {
+   answers.push('small');
+} 
+}
 console.log(answers);
+  
