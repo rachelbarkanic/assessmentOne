f545ff1a0f1b1dbd24f6e4c69c1c92492480106f
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-27 10:44:21 -0700

    password two additional checks

--- password.js
+++ password.js
@@ -1,7 +1,14 @@
-let password = "Meowmeow";
+let password = "Meowmeow123!";
 
 if (password.length > 10 && /[a-zA-Z]/.test(password) && /\d/.test(password)) {
-console.log('Success');
-} else {
-    console.log('Fail');
-}
+    console.log('Success');
+    } else if (/^\d/.test(password)) {
+        console.log('Password cannot begin with a number');
+    } else if (/\s/. test(password)) {
+      console.log('Password cannot contain a space')
+    } else {
+      console.log('Fail - password does not meet specifications: must be more than 10 characters long and contain at least one number and one letter');
+    }
+    
+    //Included a check to see if password starts with a number 
+    //Included a check to see if password contains a space
